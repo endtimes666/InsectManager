ce76347119f41021de354c569ff3fee1d306ed2f
eyefloater <gobifish@gmail.com>
2015-11-28 12:50:55 -0600

    DONE

--- src/com/gaby/Bee.java
+++ src/com/gaby/Bee.java
@@ -1,15 +1,3 @@
-/*Bee needs to have three variables:
-A String for the bee’s color
-A boolean for whether this species of bee makes honey (not all bees do)
-A String for this bee’s favorite flower
-Bee also needs a constructor to set all the variables a Bee object has. 
-And, bee needs a method called printSpeciesData() that prints out 
-all of the data for a Bee species. This method has the same name 
-as the Butterfly method, but prints out the Bee information. 
-As it prints out the information, it should print “This bee does make honey” 
-or “this bee does not make honey” instead of “true” or “false”.
- */
-
 package com.gaby;
 
 public class Bee extends Insect {
--- src/com/gaby/Butterfly.java
+++ src/com/gaby/Butterfly.java
@@ -1,14 +1,3 @@
-/*Butterfly needs to extend Insect. Butterfly needs two methods
-A String to store the butterfly’s wing color
-A String to store the butterfly’s favorite flower
-Will these be public, private, or protected? 
-		Add get and set methods for these variables.
-Also, please add a constructor that takes 4 arguments – 
-the butterfly’s name, wing color, number of wings, and favorite flower.
-Also, add a method to the Butterfly class called printSpeciesData. 
-This method should print all the info for a Butterfly object – 
-it’s name, wing color, number of wings, number of legs, and favorite flower.*/
-
 package com.gaby;
 
 public class Butterfly extends Insect {
@@ -27,7 +16,6 @@ public class Butterfly extends Insect {
 		System.out.println("Wing color: " + getWingColor());
 		System.out.println("Favorite flower: " + getFavoriteFlower());
 		System.out.println("");
-
 
 	}
 
